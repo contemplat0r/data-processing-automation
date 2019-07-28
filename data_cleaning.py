3a0af86603815d3511bfc9bf8b0ff9ded1d1d94c
Contemplat0r <uldo.forme@gmail.com>
2019-07-28 19:17:20 +0300

    Code full rewrite. Now each colum pass to 'pipe' that is a sequence of procesors (functions) which change column description

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,16 +3,17 @@ import pandas as pd
 
 import collections
 
-column_description = collections.namedtuple('column_description', ['dtype', 'value_counts', 'nans_count', 'problem', 'decision'])
-
+column_description = collections.namedtuple(
+        'column_description',
+        #['name', 'dtype', 'value_counts', 'nans_count', 'problem', 'way_fix_problem']
+        ['name', 'num_of_rows', 'dtype', 'value_counts', 'nans_count', 'problem', 'fix_problem_way']
+    )
 
 
 def get_df_extended_info(df):
 
     df_shape = df.shape
-    rows_num = df_shape[0]
     columns = df.columns
-    df_dtypes = df.dtypes
     
     columns_info = {}
     for column in columns.tolist():
@@ -25,7 +26,6 @@ def get_df_extended_info(df):
             '',
             ''
         )
-     
     return df_shape, columns_info
 
 def detect_problems_column(df_extended_info):
@@ -38,24 +38,74 @@ def detect_problems_column(df_extended_info):
     return df_shape, columns_info
 
 
+    
+def create_column_description(column_series):
+    return column_description(
+            column_series.name,
+            column_series.shape[0],
+            column_series.dtype,
+            column_series.value_counts(),
+            len(column_series[column_series.isna()]),
+            '',
+            ''
+        )
+
+def detect_column_problems(column_description):
+    if len(column_description.value_counts) == 1:
+        column_description = column_description._replace(problem='one_value')
+    if column_description.nans_count > 0:
+        column_description = column_description._replace(problem='isna')
+    return column_description
+
+def select_problem_fix_way(column_description):
+    return
+
+def data_clean_processor(df, column_processors):
+
+    columns = df.columns
+    columns_description = {}
+    for column in columns.tolist():
+        column_description = create_column_description(df[column])
+        for processor in column_processors:
+            column_description = processor(column_description)
+        columns_description[column] = column_description
+
+    return {'df_shape': df.shape, 'columns_description': columns_description}
+
+
 if __name__ == '__main__':
 
     df = pd.read_csv('train_users_2.csv')
 
+    '''
     info = get_df_extended_info(df)
     print("shape")
     print(info[0])
     print('\ninfo')
     #print(info[1])
     '''
+
+    '''
     for column_name, column_info in info[1].items():
         print('\n', column_name, '\n')
         print(column_info)
         print('\n' * 2)
+    '''
+
     '''
     detect_problems_info = detect_problems_column(info)
     for column_name, column_info in detect_problems_info[1].items():
         print('\n', column_name, '\n')
         print(column_info)
         print('\n' * 2)
+    '''
+
+    data_description = data_clean_processor(df, [detect_column_problems])
+    print(data_description['df_shape'])
+    
+    columns_description = data_description['columns_description']
+
+    for column_name, column_description in columns_description.items():
+        print(column_name)
+        print(column_description)
 
